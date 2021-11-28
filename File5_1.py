3ba02da760a0fa470a0f6e7ac6abc6721acf1c68
Vladyslav <V.Koshchavka_FIT_8_21_B_d@knute.edu.ua>
2021-11-28 20:48:35 +0300

    .

---
+++ File5_1.py
@@ -0,0 +1,44 @@
+import csv
+import sys
+zag=0
+pr=0
+my_list2=[]
+my_list1=[]
+filename = "tom_sawyer.txt"
+ryadky=[]
+fd = open(filename, "r")
+ryadky=fd.readlines()
+fd.close()
+for elem in ryadky:
+    zag=zag+len(elem)
+    pr=pr+elem.count(' ')
+    slova=elem.split(' ')
+    my_list1.append(slova)
+print(my_list1[3])
+print('pr=',pr) 
+print('sym=', zag)
+L1=len(my_list1)
+L2=0
+words_all=0
+words_unig=0
+for i in range(L1):
+    L2=len(my_list1[i])
+    for j in range (L2):
+        elem=my_list2[i][j]
+        words_all=words_all+1
+        if elem in my_list2:
+            continue
+        else:
+            my_list2.append(elem)
+#print(my_list2)
+L1=len(my_list2)
+for i in range(L1):
+    elem=my_list2[i]
+    zriz=elem[-2:]
+    if zriz=="\n":
+        my_list2[i]=elem[0:-2]
+    #print(zriz)
+words_uniq=len(my_list2)
+print(my_list2)
+print("all_words=",words_all)
+print("unique words=",words_uniq)
